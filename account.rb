eeb02ae1908c3a4a9c76271412ce28ff649f784b
Kate Shaffer <kate.shaffer@gmail.com>
2016-08-24 20:28:16 -0700

    Wave 2: Updated Account & Owner classes to create/search accounts from CSV. To do: link Accounts & Owners.

--- account.rb
+++ account.rb
@@ -1,44 +1,62 @@
+# @todo link owners & accounts
+# @todo integrate money gem
+# @todo line 24: try out rescue instead of ArgumentError? 
+
+
 require_relative 'owner'
+require 'csv'
 
 module Bank
   class Account
-    attr_accessor :balance, :id, :owner
+    attr_accessor :balance, :account_id, :owner, :open_date
 
-    def initialize(owner, start_balance) #start_balance = Fixnum (cents)
-      if start_balance.to_f >= 0 && start_balance.class == Fixnum
+    # initialize with parameters:
+    # - account_id = Fixnum
+    # - start_balance = Fixnum (cents)
+    # - open_date = Datetime
+    # - owner = Owner (if passed no Owner object, initialize a default Owner named "The_Bank")
+    def initialize(account_id, start_balance, open_date, owner = Owner.new({last_name: "The_Bank"}))
+      if start_balance.to_i >= 0 && start_balance.class == Fixnum
         @balance = start_balance
       else
         raise ArgumentError, "Start balance must be a positive integer (balance of account in cents)"
-      end
-
-      @id = rand(1000000..9999999)
-      @owner = owner
-
-      #@todo ? - rescue?
+      end # if
+      #@todo - rescue?
       #start_balance.to_f < 0 || ( start_balance.class == Float || start_balance.class == Fixnum ) puts "Rescue"
-    end
+
+      @account_id = account_id
+      @owner = owner
+      @open_date = open_date
+    end #initialize
 
     # withdraw method that accepts a single parameter which represents the amount of money that will be withdrawn.
     def withdraw(amount)
       if amount <= @balance
         @balance -= amount
       else
-        puts "Insufficient funds: Cannot withdraw $#{ amount.round(2) } from account." +
-        " Current balance is: $#{ @balance }"
+        puts "Insufficient funds: Cannot withdraw #{ amount } from account." +
+        " Current balance is: #{ @balance } cents."
       end
       return @balance
-    end
+    end #withdraw
 
     # deposit method that accepts a single parameter which represents the amount of money that will be deposited.
     def deposit(amount)
       @balance += amount
       return @balance
+    end #deposit
+
+    def self.all
+      all_accounts = {}
+      CSV.read('support/accounts.csv').each do |line|
+        all_accounts[line[0].to_i] = self.new(line[0].to_i, line[1].to_i, line[2])
+      end
+      return all_accounts
+    end # self.all
+
+    def self.find(id)
+      return self.all[id]
     end
 
-    #@todo - is this method necessary?
-    def print_balance
-      puts @balance.to_s
-    end
-
-  end #Account
-end #Bank
+  end # Account
+end # Bank
--- owner.rb
+++ owner.rb
@@ -1,8 +1,14 @@
+# @todo link owners & accounts
+# @todo line 39: separate out street number from street name
+
+require 'csv'
+
 module Bank
   class Owner
-    attr_accessor :first_name, :last_name, :street_number, :street_name, :city, :state, :zip, :phone_number, :email_address
+    attr_accessor :owner_id, :first_name, :last_name, :street_number, :street_name, :city, :state, :zip, :phone_number, :email_address
 
     def initialize(owner_info_hash)
+      @owner_id = owner_info_hash[:owner_id]
       @first_name = owner_info_hash[:first_name]
       @last_name = owner_info_hash[:last_name]
       @street_number = owner_info_hash[:street_number]
@@ -13,5 +19,36 @@ module Bank
       @phone_number = owner_info_hash[:phone_number]
       @email_address = owner_info_hash[:email_address]
     end
-  end #Owner
-end #Bank
+
+    # self.all - returns a collection of Owner instances, representing all of the Owners described in the CSV. See below for the CSV file specifications
+    #
+    # The data, in order in the CSV, consists of:
+    # ID - (Fixnum) a unique identifier for that Owner
+    # Last Name - (String) the owner's last name
+    # First Name - (String) the owner's first name
+    # Street Addess - (String) the owner's street address
+    # City - (String) the owner's city
+    # State - (String) the owner's state
+    def self.all
+      all_owners = {}
+      CSV.open('support/owners.csv').each do |line|
+        new_owner_hash = {
+          owner_id: line[0].to_i,
+          last_name: line[1],
+          first_name: line[2],
+          street_name: line[3], #@todo - separate out street number from street name
+          city: line[4],
+          state: line[5]
+        }
+        all_owners[line[0].to_i] = self.new(new_owner_hash)
+      end
+      return all_owners
+    end
+
+    # self.find(id) - returns an instance of Owner where the value of the id field in the CSV matches the passed parameter
+    def self.find(id)
+      return self.all[id]
+    end
+
+  end # Owner
+end # Bank
